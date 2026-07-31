d5f80cfcbafa89a84c15a4d23d7d1135b999ab71
testxxxxxxxxx <pppppmarcin@gmail.com>
2026-07-31 21:35:01 +0200

    Created ServerI

--- include/Server.hpp
+++ include/Server.hpp
@@ -2,15 +2,21 @@
 #define SERVER_HPP
 #include <netinet/in.h>
 namespace SocketServer {
-	class Server {
+	class ServerI {
+		virtual void init(int size) = 0;
+		virtual int acceptConnections(struct sockaddr_in* caddr) = 0;
+		virtual void closeSocket() = 0;
+		virtual void setnonblock(int fd) = 0;
+	};
+	class Server : public ServerI {
 		int fd, port;
 		public:
 			Server(int port): port(port) {};
 			virtual ~Server() = default;
-			void init(int size);
-			int acceptConnections(struct sockaddr_in* caddr);
-			void closeSocket();
-			void setnonblock(int fd);
+			void init(int size) override;
+			int acceptConnections(struct sockaddr_in* caddr) override;
+			void closeSocket() override;
+			void setnonblock(int fd) override;
 	};
 };
 #endif
